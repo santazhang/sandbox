4e44642a64668b25955a7951c9f769e3039efb7b
Santa Zhang <santa1987@gmail.com>
2009-04-11 23:43:21 +0800

    Added Mono C# project and OpenMP project

---
+++ monodev/HelloMono/HelloMono/Main.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace HelloMono {
+	
+	class MainClass {
+
+		public static void Main(string[] args) {
+			for (int i = 0; i < 20; i++) {
+				Console.WriteLine("Hello World! {0}", i);
+			}
+		}
+	}
+}
