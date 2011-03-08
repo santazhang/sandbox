0dc656e7483e2091ae6888a65009c9622574787f
Santa Zhang <santa1987@gmail.com>
2011-03-08 12:47:33 +0800

    added xelatex apidoc template from Justin Ye Feng

---
+++ yefeng_api_doc/clean.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+# This script is used to cleanup the generarted files.
+
+import os
+
+def my_exec(cmd):
+  print "[cmd] %s" % cmd
+  os.system(cmd)
+
+my_exec("rm -f *.log *.swp *.toc *.idx *.nav *.out *.snm *.dvi *.aux")
+
+for f in os.listdir("."):
+  if f.endswith(".pdf"):
+    tex_fn = f[:-4] + ".tex"
+    if os.path.exists(tex_fn):
+      my_exec("rm -f %s" % f)
