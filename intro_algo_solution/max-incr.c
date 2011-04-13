7ce0e683c0d729132b1bdbce9e527eef2e9b1312
Santa Zhang <santa1987@gmail.com>
2011-04-13 14:24:29 +0800

    added solution to dynamic programming problem

--- intro_algo_solution/max-incr.c
+++ intro_algo_solution/max-incr.c
@@ -1,6 +1,39 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-  printf("This shall be done!\n");
+static int solve(int* a, int len) {
+  int m, i, j;
+  int *s = (int *) malloc(len * sizeof(int));
+  for (i = 0; i < len; i++) {
+    s[i] = 1;
+  }
+  for (i = 1; i < len; i++) {
+    for (j = i - 1; j >= 0; j--) {
+      if (a[i] > a[j] && s[i] < s[j] + 1) {
+        s[i] = s[j] + 1;
+      }
+    }
+  }
+  m = s[0];
+  for (i = 1; i < len; i++) {
+    if (m < s[i]) {
+      m = s[i];
+    }
+  }
+  free(s);
+  return m;
+}
+
+int main(int argc, char* argv[]) {
+  int i, len;
+  int *a;
+  scanf("%d", &len);
+  a = (int *) malloc(len * sizeof(int));
+  for (i = 0; i < len; i++) {
+    scanf("%d", &a[i]);
+  }
+  printf("%d\n", solve(a, len));
+  free(a);
   return 0;
 }
+
