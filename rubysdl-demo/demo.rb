15336bc2180647a67ffc1c65577cb9f9e10f6a73
Santa Zhang <santa1987@gmail.com>
2012-05-05 20:21:05 +0800

    [auto] update sandbox/rake_workspace/workspace.conf to newest version

--- rubysdl-demo/demo.rb
+++ rubysdl-demo/demo.rb
@@ -3,24 +3,28 @@
 require 'rubygems'
 require 'sdl'
 
+
 SDL.init( SDL::INIT_VIDEO ) 
-screen = SDL::Screen.open( 640, 480, 16, SDL::SWSURFACE ) 
+screen = SDL.setVideoMode( 640, 480, 0, SDL::SWSURFACE ) 
+image = SDL::Surface.load( 'my-avatar.png' )
+image.setColorKey( SDL::SRCCOLORKEY, image.getPixel(0,0) ) 
+image = image.displayFormat 
+
+image_x = 20
 
 loop do
-  sleep 0.001
   while event = SDL::Event2.poll # (1)
-	puts event
     case event # (2)
     when SDL::Event2::Quit 
-	puts "quit"
       exit # (3)
     when SDL::Event2::KeyDown
       if event.sym == SDL::Key::SPACE then # (4)
-puts "space"
+        image_x += 10
       end
     end
   end
 
-#  screen.fillRect( 0, 0, 640, 480, [ 0, 0, 0 ] )
-#  screen.updateRect( 0, 0, 0, 0 )
+  screen.fillRect( 0, 0, 640, 480, [ 0, 0, 0 ] )
+  screen.put( image, image_x, 200 )
+  screen.updateRect( 0, 0, 0, 0 )
 end
