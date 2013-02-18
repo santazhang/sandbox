6355200c852084bb5dcfd418e85c2daf22e934cb
Santa Zhang <santa1987@gmail.com>
2013-02-18 15:48:54 -0500

    use make to build small objc projects

---
+++ bigint/bi.h
@@ -0,0 +1,8 @@
+#pragma once
+
+struct __bi;
+typedef struct __bi* bi;
+
+bi bi_new();
+
+void bi_free();
--- bigint/test.c
+++ bigint/test.c
@@ -1,56 +1,10 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
-#ifndef __APPLE__
-#include <malloc.h>
-#endif
-
-#include "bigint.h"
-
-void do_test(char* a, char* op, char* b) {
-  bigint va;
-  bigint vb;
-  char* buf = NULL;
-  bigint_init(&va);
-  bigint_init(&vb);
-
-  bigint_from_string(&va, a);
-  bigint_from_string(&vb, b);
-
-  if (strcmp(op, "+") == 0) {
-    bigint_add_by(&va, &vb);
-  } else if (strcmp(op, "-") == 0) {
-    bigint_sub_by(&va, &vb);
-  } else if (strcmp(op, "times") == 0) {
-    bigint_mul_by(&va, &vb);
-  } else if (strcmp(op, "pow") == 0) {
-    int v;
-    bigint_to_int(&vb, &v);
-    bigint_pow_by_int(&va, v);
-  } else if (strcmp(op, "div") == 0) {
-    bigint_div_by(&va, &vb);
-  } else if (strcmp(op, "mod") == 0) {
-    bigint_mod_by(&va, &vb);
-  } else {
-    printf("err: op not known: %s\n", op);
-  }
-
-  buf = (char *) malloc(bigint_string_length(&va) + 10);
-  bigint_to_string(&va, buf);
-  printf("va = %s\n", buf);
-
-  bigint_release(&va);
-  bigint_release(&vb);
-  free(buf);
-}
+#include "bi.h"
 
 int main(int argc, char* argv[]) {
-  if (argc != 4) {
-    printf("usage: test a op b\n");
+    printf("This shall be done!\n");
+    bi v1 = bi_new();
+    bi_free(v1);
     return 0;
-  }
-  do_test(argv[1], argv[2], argv[3]);
-  return 0;
 }
-
