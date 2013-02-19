aa273200d49db090de52675faacb3d56c0610ec5
Santa Zhang <santa1987@gmail.com>
2013-02-19 01:37:03 -0500

    cleanup themes

---
+++ RainmeterScreenlet/install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+rm -rf ~/.screenlets/RainmeterScreenlet
+mkdir -p ~/.screenlets/RainmeterScreenlet
+cp -r RainmeterScreenlet.py icon.png themes ~/.screenlets/RainmeterScreenlet
+
--- rainmeter_adapter/install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-rm -rf ~/.screenlets/RainmeterAdapter
-mkdir -p ~/.screenlets/RainmeterAdapter
-cp -r *.py icon.png themes ~/.screenlets/RainmeterAdapter
-
