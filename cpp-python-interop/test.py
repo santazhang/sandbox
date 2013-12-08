ec790dd3c431d64f72fc6522791548b881b1c6db
Santa Zhang <santa1987@gmail.com>
2013-12-08 11:30:02 -0500

    cpp-python interop: checking multithreading issue

--- cpp-python-interop/test.py
+++ cpp-python-interop/test.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python
 
 import tinymathpy
+import time
 
 print dir(tinymathpy)
 
 print tinymathpy.gcd(5, 3)
+
+def f(v):
+    print v
+
+print tinymathpy.mt_callback(f)
+
+for i in range(100):
+    time.sleep(1)
--- cpp-python-interop/tinymath-py.cc
+++ cpp-python-interop/tinymath-py.cc
@@ -1,5 +1,6 @@
 #include <stdio.h>
-
+#include <pthread.h>
+#include <string>
 #include <Python.h>
 
 #include "tinymath.h"
@@ -20,8 +21,37 @@ static PyObject* tinymath_gcd(PyObject* self, PyObject* args) {
     return Py_BuildValue("i", result);
 }
 
+static void* mt_callback(void* arg) {
+    PyObject* func = (PyObject *) arg;
+
+    Py_XINCREF(func);
+    std::string s = "from C";
+    PyObject* params = Py_BuildValue("(s)", &s[0]);
+    PyObject_CallObject(func, params);
+    //Py_XDECREF(func);
+
+    pthread_exit(NULL);
+    return NULL;
+}
+
+static PyObject* tinymath_mt_callback(PyObject* self, PyObject* args) {
+    PyObject* func;
+    if (!PyArg_ParseTuple(args, "O", &func))
+        return NULL;
+
+    for (int i = 0; i < 100; i++) {
+        pthread_t th;
+        pthread_create(&th, NULL, mt_callback, func);
+    }
+
+    mt_callback(func);
+
+    Py_RETURN_NONE;
+}
+
 static PyMethodDef TinyMathMethods[] = {
     {"gcd", tinymath_gcd, METH_VARARGS, "get gcd value of a and b"},
+    {"mt_callback", tinymath_mt_callback, METH_VARARGS, NULL},
     {NULL, NULL, 0, NULL}
 };
 
