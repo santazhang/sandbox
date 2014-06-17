86c01f4a012286e188bbb7a9f7ea50527ccb6db3
Santa Zhang <santa1987@gmail.com>
2014-06-17 14:17:27 -0400

    add go perf test

---
+++ omniperf/go.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	const loop_n = 1000 * 1000
+	counter := 0
+	start := time.Now()
+	crap_sum := 0
+	for counter < loop_n {
+		counter++
+	}
+	nanosec := time.Since(start).Nanoseconds()
+	qps := float64(counter) * 1000 * 1000 * 1000 / float64(nanosec);
+	fmt.Printf("i++: %.0f / s\n", qps);
+}
