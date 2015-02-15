b6e393cc896d06e00762daf2a3b9a0dab17312c7
Santa Zhang <santa1987@gmail.com>
2015-02-15 12:49:44 -0500

    add google io 2012 slidses

--- love2d/play/main.lua
+++ love2d/play/main.lua
@@ -2,7 +2,16 @@ local debug = true
 love.load = function(arg) end
 love.update = function(dt)
   if love.keyboard.isDown('escape') then
-    return love.event.push('quit')
+    local r = love.window.showMessageBox('Quit?', 'Are you sure?', {
+      'YES',
+      'NO'
+    })
+    if r == 1 then
+      return love.event.push('quit')
+    end
+  elseif love.keyboard.isDown('f') then
+    local fullscreen, _ = love.window.getFullscreen()
+    return love.window.setFullscreen((not fullscreen), 'desktop')
   end
 end
 love.draw = function()
--- love2d/play/main.moon
+++ love2d/play/main.moon
@@ -4,7 +4,13 @@ love.load = (arg) ->
 
 love.update = (dt) ->
     if love.keyboard.isDown 'escape'
-        love.event.push 'quit'
+        r = love.window.showMessageBox 'Quit?', 'Are you sure?', {'YES', 'NO'}
+        if r == 1
+            love.event.push 'quit'
+
+    elseif love.keyboard.isDown 'f'
+        fullscreen, _ = love.window.getFullscreen!
+        love.window.setFullscreen (not fullscreen), 'desktop'
 
 love.draw = ->
     love.graphics.print "Hello world!", 400, 300
