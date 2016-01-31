53e8796a7b4e3aadd0d076d33299e72d86ede720
Santa Zhang <santa1987@gmail.com>
2016-01-31 09:52:52 -0500

    add tornado and missing stuff

--- overlord/build.sh
+++ overlord/build.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-rm -rf "$(dirname $0)/Crypto"
+rm -rf "$(dirname $0)/Crypto" "$(dirname $0)/tornado"
+
 cd "$(dirname $0)/pycrypto-2.6.1"
 python setup.py build
 mv build/lib.*/Crypto ..
 cd ..
 
+cd "$(dirname $0)/tornado-4.3.0"
+python setup.py build
+mv build/lib.*/tornado ..
+cd ..
+
 echo ----
 
-python -c "import spur"
+python -c "import spur; import tornado"
