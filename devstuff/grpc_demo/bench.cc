1188dae658bea2c1493feecbe4ac1ea97556244e
Santa Zhang <santa1987@gmail.com>
2016-01-30 21:15:39 -0500

    async grpc tests

--- devstuff/grpc_demo/bench.cc
+++ devstuff/grpc_demo/bench.cc
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <signal.h>
 #include <sys/time.h>
+#include <pthread.h>
 
 #include <thread>
+#include <vector>
 #include <atomic>
+#include <iostream>
 
 #include <grpc++/grpc++.h>
 
@@ -15,7 +18,7 @@ using namespace grpc;
 
 
 const char *svr_addr = "127.0.0.1:6789";
-int byte_size = 10;
+int byte_size = 1;
 int seconds = 10;
 int outgoing_requests = 1000;
 int client_threads = 8;
@@ -119,14 +122,169 @@ void run_as_sync_client() {
            done, t.elapsed(), done / t.elapsed());
 }
 
+void async_server_handle_rpcs(ServerCompletionQueue* cq, Bench::AsyncService* service) {
+    class CallData {
+    public:
+        CallData(Bench::AsyncService* service, ServerCompletionQueue* cq)
+                : service_(service), cq_(cq), responder_(&ctx_), status_(CREATE) {
+            Proceed();
+        }
+
+        void Proceed() {
+            if (status_ == CREATE) {
+                service_->Requestadd(&ctx_, &request_, &responder_, cq_, cq_, this);
+                status_ = PROCESS;
+            } else if (status_ == PROCESS) {
+                new CallData(service_, cq_);
+
+                // do work
+                int s = 0;
+                for (const auto& v : request_.v()) {
+                    s += v;
+                }
+                reply_.set_v(s);
+
+                // reply
+                responder_.Finish(reply_, Status::OK, this);
+                // cout << "done in thread: " << std::this_thread::get_id() << endl;
+
+                status_ = FINISH;
+            } else {
+                GPR_ASSERT(status_ == FINISH);
+                delete this;
+            }
+        }
+
+    private:
+        Bench::AsyncService* service_;
+        ServerCompletionQueue* cq_;
+        ServerContext ctx_;
+        Request request_;
+        Response reply_;
+        ServerAsyncResponseWriter<Response> responder_;
+
+        enum CallStatus { CREATE, PROCESS, FINISH };
+        CallStatus status_;  // The current serving state.
+    };
+
+    new CallData(service, cq);
+
+    for (;;) {
+        void* tag;
+        bool ok;
+        cq->Next(&tag, &ok);
+        GPR_ASSERT(ok);
+        static_cast<CallData*>(tag)->Proceed();
+    }
+}
+
 void run_as_async_server() {
-    printf("%s not implemented yet!\n", __PRETTY_FUNCTION__);
-    exit(1);
+    ServerBuilder builder;
+    builder.AddListeningPort(svr_addr, grpc::InsecureServerCredentials());
+
+    Bench::AsyncService service;
+    builder.RegisterAsyncService(&service);
+
+    vector<ServerCompletionQueue*> cqs;
+    for (int i = 0; i < worker_threads; i++) {
+        cqs.push_back(builder.AddCompletionQueue().release());
+    }
+
+    std::unique_ptr<Server> server(builder.BuildAndStart());
+
+    printf("server started at %s\n", svr_addr);
+
+    std::thread** threads = new std::thread*[worker_threads];
+    for (int i = 0; i < worker_threads; i++) {
+        auto* cq = cqs[i];
+        threads[i] = new std::thread([cq, &service] {
+            async_server_handle_rpcs(cq, &service);
+        });
+    }
+
+    for (int i = 0; i < worker_threads; i++) {
+        threads[i]->join();
+        delete threads[i];
+    }
+    delete[] threads;
+}
+
+void run_as_async_client_work() {
+    printf("connecting to %s\n", svr_addr);
+
+    auto channel = CreateChannel(svr_addr, InsecureChannelCredentials());
+    auto stub = Bench::NewStub(channel);
+
+    Request request;
+    for (int j = 0; j < byte_size; j++) {
+        request.add_v(1);
+    }
+
+    Timer t;
+    t.start();
+    CompletionQueue cq;
+
+    struct call_info {
+        ClientContext* context = nullptr;
+        Status* status = nullptr;
+        Response* response = nullptr;
+        ClientAsyncResponseReader<Response>* rpc = nullptr;
+
+        ~call_info() {
+            delete context;
+            delete status;
+            delete response;
+            delete rpc;
+        }
+    };
+
+    auto make_one_call = [&cq, &stub, request] {
+        call_info* info = new call_info;
+        info->context = new ClientContext;
+        info->status = new Status;
+        info->response = new Response;
+        info->rpc = stub->Asyncadd(info->context, request, &cq).release();
+        info->rpc->Finish(info->response, info->status, static_cast<void*>(info));
+    };
+
+    for (int i = 0; i < outgoing_requests; i++) {
+        make_one_call();
+    }
+
+    for (;;) {
+        void* got_tag;
+        bool ok = false;
+        cq.Next(&got_tag, &ok);
+        GPR_ASSERT(ok);
+        call_info* info = static_cast<call_info*>(got_tag);
+        delete info;
+
+        int done = done_count++;
+        if (done % 10 == 0) {
+            if (t.elapsed() > seconds) {
+                break;
+            }
+        }
+        make_one_call();
+    }
 }
 
 void run_as_async_client() {
-    printf("%s not implemented yet!\n", __PRETTY_FUNCTION__);
-    exit(1);
+    std::thread** threads = new std::thread*[client_threads];
+    Timer t;
+    t.start();
+    for (int i = 0; i < client_threads; i++) {
+        threads[i] = new std::thread(run_as_async_client_work);
+    }
+    for (int i = 0; i < client_threads; i++) {
+        threads[i]->join();
+        delete threads[i];
+    }
+    t.stop();
+    delete[] threads;
+    int done = done_count.load();
+    printf("  *** done count = %d, elapsed = %.2lf sec, qps = %.0f\n",
+           done, t.elapsed(), done / t.elapsed());
 }
 
 int main(int argc, char* argv[]) {
