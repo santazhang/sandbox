c6243cab653dfb14dd30a2f780cf9bb4d6358add
Santa Zhang <santa1987@gmail.com>
2016-03-21 16:02:26 -0400

    poorman_ci frequency control

---
+++ .poorman_ci/frequency_control.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# Usage: frequency_control.sh project min_interval src_tree command
+
+if [ "$#" -lt 3 ]; then
+    echo "Usage: frequency_control.sh project min_interval src_tree command"
+    exit 1
+fi
+
+project=$1
+shift
+min_interval=$1
+shift
+src_tree=$1
+shift
+
+SENTINEL_DIR=/tmp/poorman_ci_frequency_control/
+mkdir -p $SENTINEL_DIR
+
+OS_UNAME=`uname`
+if [[ "$OS_UNAME" == 'Linux' ]]; then
+    MD5_CMD="md5sum"
+elif [[ "$OS_UNAME" == 'Darwin' ]]; then
+    MD5_CMD="md5"
+else
+    echo "Unsupported OS: $OS_UNAME"
+    exit 1
+fi
+
+project_md5=`echo "$project" | $MD5_CMD`
+src_tree_md5=`find "$src_tree" -type f | sort | xargs -n 1 cat | $MD5_CMD`
+sentinel_fn="$SENTINEL_DIR/$project_md5-$src_tree_md5"
+
+if find $SENTINEL_DIR -type f -Btime -$min_interval | \
+        grep -q "$project_md5-$src_tree_md5"; then
+
+    # Found file, so the min_interval has not passed yet.
+    echo "  *** Skipping command '$@' ($min_interval interval)"
+    exit 0
+
+else
+    rm -f $sentinel_fn
+    touch $sentinel_fn
+    echo "  *** Will run command '$@'"
+    if $@; then
+        touch $sentinel_fn
+        echo "  *** Created sentinel file $sentinel_fn"
+        echo "  *** Will not re-run until $min_interval passed"
+    else
+        rm -f $sentinel_fn
+        echo "  *** Command '$@' failed"
+        echo "  *** Removing sentinel file $sentinel_fn (if exists)"
+    fi
+fi
