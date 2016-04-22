3ce23e5ae010f747710927c72fc37fd666376b51
Santa Zhang <santa1987@gmail.com>
2016-04-22 15:25:57 -0400

    add build_jar.sh

---
+++ yarn_workflow_scheduler/build_jar.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+HADOOP_DIR="/Users/santa/Downloads/k/hadoop-2.7.2"
+. $HADOOP_DIR/libexec/hadoop-config.sh
+
+mkdir -p out/production
+
+javac src/yarn_workflow_scheduler/*.java -cp $CLASSPATH:src \
+    -d out/production
+
+jar -cf yarn_workflow_scheduler.jar -C out/production/ .
