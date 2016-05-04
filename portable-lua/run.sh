8d52ede61f4b0cf74debb3894398fd57ad06b673
Santa Zhang <santa1987@gmail.com>
2016-05-04 15:54:32 +0000

    minor update

--- portable-lua/run.sh
+++ portable-lua/run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 DIR=$(dirname $0)
-LD_LIBRARY_PATH=$DIR:$LD_LIBRARY_PATH $DIR/ld-linux-x86-64.so.2 $DIR/lua
+LD_DEBUG=files,statistics LD_LIBRARY_PATH=$DIR:$LD_LIBRARY_PATH $DIR/ld-linux-x86-64.so.2 \
+    --inhibit-cache --inhibit-rpath $DIR/lua $DIR/lua
