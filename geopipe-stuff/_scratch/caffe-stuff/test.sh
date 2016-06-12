2443d2219fa66168afad1c5c3072425254c867b4
Santa Zhang <santa1987@gmail.com>
2016-06-12 09:14:35 -0400

    training scripts

--- geopipe-stuff/_scratch/caffe-stuff/test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-/Users/santa/Downloads/caffe/build/tools/caffe test \
-    -model layer.prototxt -weights _iter_36625.caffemodel \
-        -iterations 1000
--- geopipe-stuff/training/step4_do_training.py
+++ geopipe-stuff/training/step4_do_training.py
@@ -5,7 +5,12 @@ from __future__ import print_function
 import os
 os.chdir(os.path.dirname(__file__))
 
-from training_configurations import *
 from utils import *
 
-print("  *** THIS SHALL BE DONE!")
+for d1 in os.listdir("06_training_output/"):
+    retrain_script = os.path.join("06_training_output/", d1, "retrain.sh")
+    test_script = os.path.join("06_training_output/", d1, "test.sh")
+    if os.path.exists(retrain_script):
+        run_cmd(retrain_script)
+    if os.path.exists(test_script):
+        run_cmd(test_script)
