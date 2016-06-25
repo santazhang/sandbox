1953b0fe3ca9926444e17a46c57e13ecaf98c29f
Santa Zhang <santa1987@gmail.com>
2016-06-25 16:54:06 -0400

    incubated cleaner-chrome-browsing

--- cleaner-chrome-browsing/clean-wikia.js
+++
@@ -1,7 +0,0 @@
-function removeAllIframes() {
-    Array.prototype.slice.call(document.querySelectorAll('iframe')).forEach(function(element, index, array) {
-        element.parentNode.removeChild(element);
-    });
-}
-
-setInterval(removeAllIframes, 1000);
