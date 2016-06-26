4d8961be242a1e02d75102e18c3ed85904cb572a
Santa Zhang <santa1987@gmail.com>
2016-06-25 21:56:54 -0400

    ensure built image database has same order of test data

--- geopipe-stuff/training/step3_rebuild_training_database.py
+++ geopipe-stuff/training/step3_rebuild_training_database.py
@@ -9,14 +9,13 @@ import random
 from training_configurations import *
 from utils import *
 
-for channels in TRAINING_COMBINATIONS:
-    config_name = "-".join(channels)
-    if config_name not in ["R-G-B", "IR", "LAB_A", "CMYK_Y"]:
-        print("  *** NOT SUPPORTED YET: %s" % config_name)
-        continue
-    print("Building training data for: %s" % config_name)
-    for tile_size in TILE_SIZES:
-        print("Tile size: %dx%d" % (tile_size, tile_size))
+for tile_size in TILE_SIZES:
+    allowed_fnames = set()
+    store_fnames_list = {}
+    print("Tile size: %dx%d" % (tile_size, tile_size))
+
+    for channels in TRAINING_COMBINATIONS:
+        config_name = "-".join(channels)
         if config_name == "R-G-B":
             channels = "rgb"
         elif config_name == "IR":
@@ -26,24 +25,72 @@ for channels in TRAINING_COMBINATIONS:
         elif config_name == "CMYK_Y":
             channels = "cmyk_y"
         else:
+            print("  *** NOT SUPPORTED YET: %s" % config_name)
             continue
 
+        print("Scanning training data for: %s (tile size: %dx%d)" % (config_name, tile_size, tile_size))
         tree_tile_dir = "04_training_data_preview/tree_%dx%d_%s" % (tile_size, tile_size, channels)
         not_tree_tile_dir = "04_training_data_preview/not_tree_%dx%d_%s" % (tile_size, tile_size, channels)
         tree_data_files = []
         not_tree_data_files = []
+        image_fnames_set = set()
         for root, dnames, fnames in os.walk(tree_tile_dir):
             for fn in fnames:
                 if fn.startswith(".") or not fn.endswith(".png"):
                     continue
                 fpath = os.path.abspath(os.path.join(root, fn))
                 tree_data_files += fpath,
+                image_fnames_set.add(fn)
         for root, dnames, fnames in os.walk(not_tree_tile_dir):
             for fn in fnames:
                 if fn.startswith(".") or not fn.endswith(".png"):
                     continue
                 fpath = os.path.abspath(os.path.join(root, fn))
                 not_tree_data_files += fpath,
+                image_fnames_set.add(fn)
+        if len(allowed_fnames) == 0:
+            allowed_fnames = image_fnames_set
+        else:
+            allowed_fnames = allowed_fnames.intersection(image_fnames_set)
+        print("%d tree and %d not_tree files scanned" % (len(tree_data_files), len(not_tree_data_files)))
+        store_fnames_list["tree~" + config_name] = tree_data_files
+        store_fnames_list["not_tree~" + config_name] = not_tree_data_files
+
+
+    for channels in TRAINING_COMBINATIONS:
+        config_name = "-".join(channels)
+        if config_name == "R-G-B":
+            channels = "rgb"
+        elif config_name == "IR":
+            channels = "ir"
+        elif config_name == "LAB_A":
+            channels = "lab_a"
+        elif config_name == "CMYK_Y":
+            channels = "cmyk_y"
+        else:
+            print("  *** NOT SUPPORTED YET: %s" % config_name)
+            continue
+
+        print("Building training data for: %s (tile size: %dx%d)" % (config_name, tile_size, tile_size))
+        tree_data_files = store_fnames_list["tree~" + config_name]
+        not_tree_data_files = store_fnames_list["not_tree~" + config_name]
+
+        ignore_image_cnt = 0
+        for L in [tree_data_files, not_tree_data_files]:
+            idx = 0
+            while idx < len(L):
+                fn = os.path.basename(L[idx])
+                if fn not in allowed_fnames:
+                    ignore_image_cnt += 1
+                    L[idx] = L[-1]
+                    del L[-1]
+                    continue
+                idx += 1
+        if ignore_image_cnt > 0:
+            print("Ignored %d images" % ignore_image_cnt)
+        tree_data_files.sort()
+        not_tree_data_files.sort()
+
         per_group_image_samples = min(len(not_tree_data_files), len(tree_data_files))
         per_group_image_samples = per_group_image_samples / TRAINING_BATCH_SIZE * TRAINING_BATCH_SIZE
         num_test_images = TEST_BATCHES * TRAINING_BATCH_SIZE
@@ -51,6 +98,7 @@ for channels in TRAINING_COMBINATIONS:
             print("  *** Too few training sample images found (%d, at least %d wanted)" % (per_group_image_samples, 2 * num_test_images))
             continue
         print("Choose %d images (for each group) from %d not-tree tiles and %d tree tiles" % (per_group_image_samples, len(not_tree_data_files), len(tree_data_files)))
+        random.seed(os.getpid())
         random.shuffle(not_tree_data_files)
         random.shuffle(tree_data_files)
         not_tree_data_files = not_tree_data_files[:per_group_image_samples]
@@ -85,9 +133,9 @@ for channels in TRAINING_COMBINATIONS:
         run_cmd("rm -rfv '05_training_data/%dx%d-%s-test-lmdb/'" % (tile_size, tile_size, config_name))
         run_cmd("rm -rfv '05_training_data/%dx%d-%s-train-lmdb/'" % (tile_size, tile_size, config_name))
 
-        run_cmd("../deps/_build/caffe/build/tools/convert_imageset --backend=lmdb --shuffle " + use_gray_if_necessary +
+        run_cmd("../deps/_build/caffe/build/tools/convert_imageset --backend=lmdb " + use_gray_if_necessary +
                 " / %s 05_training_data/%dx%d-%s-test-lmdb/" % (test_list_txt_fn, tile_size, tile_size, config_name))
-        run_cmd("../deps/_build/caffe/build/tools/convert_imageset --backend=lmdb --shuffle " + use_gray_if_necessary +
+        run_cmd("../deps/_build/caffe/build/tools/convert_imageset --backend=lmdb " + use_gray_if_necessary +
                 " / %s 05_training_data/%dx%d-%s-train-lmdb/" % (train_list_txt_fn, tile_size, tile_size, config_name))
 
         os.remove(test_list_txt_fn)
