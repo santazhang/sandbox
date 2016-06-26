2b2ca3d649cc64d62cff236532dbabeb98461045
Santa Zhang <santa1987@gmail.com>
2016-06-25 21:59:39 -0400

    minor

--- geopipe-stuff/training/training_configurations.py
+++ geopipe-stuff/training/training_configurations.py
@@ -18,8 +18,7 @@ TRAINING_COMBINATIONS = [
     # ("LAS_Z_STDDEV_5",),
     # ("LAS_Z_STDDEV_10",),
     # ("LAS_Z_STDDEV_15",),
-    ("LAS_Z_STDDEV_20",),
+    # ("LAS_Z_STDDEV_20",),
     # ("LAS_Z_STDDEV_25",),
     ("R", "G", "B"),
-    ("R", "G", "B", "IR", "LAB_A", "HSB_S", "CMYK_Y", "LAS_Z_STDDEV_20"),
 ]
