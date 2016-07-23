7cd8bc1c940f81e5f94810db5890f3ea827ee6f7
Santa Zhang <santa1987@gmail.com>
2016-07-22 21:53:25 -0400

    model: rgb+las_z

--- geopipe-stuff/find_trees/caffe_models.h
+++ geopipe-stuff/find_trees/caffe_models.h
@@ -12,5 +12,6 @@ namespace find_trees {
 
 caffe::Net<float>* get_caffe_net_7x7_rgb_1();
 caffe::Net<float>* get_caffe_net_7x7_rgb_lab_a_1();
+caffe::Net<float>* get_caffe_net_7x7_rgb_las_z_hint_1();
 
 }  // namespace find_trees
