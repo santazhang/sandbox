536ee7e7b622d825c3fd7bd20fd533d926032dde
Santa Zhang <santa1987@gmail.com>
2016-12-21 00:00:18 -0500

    cleanup

--- float_compression/nn_lossy.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-void nn_compress(const float* farr, int n_floats, char** compressed, int* compressed_bytes);
-
-void nn_decompress(const char* compressed, int compressed_bytes, float* farr, int n_floats);
